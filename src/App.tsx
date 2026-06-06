34a526cd7ce151a4c41334e29abf1cba29122f5c
Aijan-Abil <arstanlovaaijan@gmail.com>
2026-06-06 21:43:56 +0600

    TASK-17

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-
+import { ImportChinaButton } from "./features/import-excel/ui/importchina-button";
 const App = () => {
+
   return (
-    <>
-    </>
+    <div className="p-20">
+        <ImportChinaButton 
+          previewData={[]}/>
+    </div>
   );
 }
 export default App;
---
+++ src/features/import-excel/ui/importchina-button.tsx
@@ -0,0 +1,52 @@
+import axios from 'axios';
+import { Button } from './button';
+import { useState } from 'react';
+
+interface ImportChinaButtonProps {
+  previewData: Array<{customer_code:string ,product_code:string}>
+  
+}
+
+export const ImportChinaButton: React.FC<ImportChinaButtonProps> = ({ previewData }) => {
+ const [loading ,setLoading] = useState(false)
+  const handleImport = async () => {
+    if (!previewData || previewData.length === 0) {
+      alert('Ошибка: Выберите файл для импорта!');
+      return;
+    }
+
+    try {
+      setLoading(true)
+      const response = await axios.post(
+       'http://localhost:3000/product/import-china',previewData
+      );
+
+      if (response.status === 200 || response.status === 201) {
+        alert(response.data.message || 'Успешно импортировано!');
+        
+      }
+    } catch (error: unknown) {      
+      if (axios.isAxiosError(error)) {
+        const serverMessage = error.response?.data?.message;
+        const errorMessage = Array.isArray(serverMessage) 
+          ? serverMessage.join(', ') 
+          : serverMessage || 'Произошла ошибка при импорте';
+
+        alert(`Ошибка сервера: ${errorMessage}`);
+      } else {
+        alert('Произошла непредвиденная ошибка клиента');
+      }
+    } finally{
+      setLoading(false)
+    }
+  };
+
+  return (
+      <Button 
+        onClick={handleImport}
+        disabled={previewData.length === 0 || loading} 
+        className="h-9 w-40 text-(--text-light) hover:text-(--text-dark)"     >
+        {loading ?"Сохранение..." : "Импортировать"}
+         </Button>
+  );
+};
