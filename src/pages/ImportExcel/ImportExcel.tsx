2850545139748d66f1588a5a495b4c74fb6c272f
Zholdoshbekova <zholdoshbekovaaigerim5@gmail.com>
2026-06-09 05:44:53 +0600

    fix: tailwindcss

--- src/pages/ImportExcel/ImportExcel.tsx
+++ src/pages/ImportExcel/ImportExcel.tsx
@@ -1,23 +1,21 @@
 import { useState } from 'react';
-import './ImportExcel.css';
+
 
 export const ImportExcel = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const handleImportSuccess = () => {
-    setIsModalOpen(true);
-  };
+  const [isModalOpen, setIsModalOpen] = useState(true);
+  
 
   return (
-    <div className="import-container">
-      <button className="import-click" onClick={handleImportSuccess}>Импортировать Excel</button>
+    <div className="p-24 flex justify-end">
       
       {isModalOpen && (
-        <div className="import-overlay" onClick={() => setIsModalOpen(false)}>
-          <div className="import-content" onClick={(e) => e.stopPropagation()}>
-            <button className="import-btn" onClick={() => setIsModalOpen(false)}> x </button>
-            <p className="import-title">Товары успешно импортированы!</p>
-            <p className="import-status">Статус: На складе в Китае</p>
+        <div className="fixsed inset-0 flex justify-center items-center z-[9999] " onClick={() => setIsModalOpen(false)}>
+          <div className=" relative bg-[var(--text-light)] p-8 h-[100px] w-sm max-w-sm flex rounded-2xl text-center shadow-xl m-4 " onClick={(e) => e.stopPropagation()}>
+            <button className="absolute top-2 right-4 py-2 px-4 text-[var(--bg-dark)] text-xl cursor-pointer" onClick={() => setIsModalOpen(false)}> x </button>
+            <div className="flex flex-col justify-center items-center text-center w-full mt-6 px-6">
+               <p className="font-regular text-[#111827] mb-2">Товары успешно импортированы!</p>
+               <p className="text-sm text-[#111827]">Статус: На складе в Китае</p>
+            </div>  
           </div>
         </div>
       )}
