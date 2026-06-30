b65fd1c82d05f61553fd97fd7b23aa3058382b9a
Elza-Wun <elsawunderful@gmail.com>
2026-06-30 15:21:40 +0600

    Чуть исправлен стиль

--- src/features/import-excel/ui/ButtonUni.tsx
+++ src/features/import-excel/ui/ButtonUni.tsx
@@ -10,7 +10,7 @@ export const Button = ({isLoading,disabled, children,className='', ...props}:But
     <button
       disabled={disabled || isLoading}
       className={`
-      bg-(--bg-dark) flex p-1 hover:bg-(--bg-light) rounded-xl  justify-center items-center 
+      bg-(--bg-dark) flex p-1 hover:bg-(--bg-dark) rounded-xl  justify-center items-center 
       transition-all duration-200 focus:outline-none focus:ring-2 focus:ring-lime-500 focus:ring-offset-2 
         ${className}
       `}
